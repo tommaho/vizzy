e1cde706af1dac5eb7f4089b1e0ffacef00bdd5f
tommaho <tom@tommaho.com>
2024-02-12 16:20:15 -0500

    getting structure set up

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return render_template('index.html', name='Tom')
+@app.route('/data')
+def data():  # put application's code here
+    return render_template('data.html', name='Tom')
+@app.route('/viz')
+def viz():  # put application's code here
+    return render_template('viz.html', name='Tom')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 
 if __name__ == '__main__':
