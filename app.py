65a2ab024b390c14cc652be037de3ee956adbd6f
tommaho <tom@tommaho.com>
2024-02-14 20:11:16 -0500

    configure for file upload

--- app.py
+++ app.py
@@ -4,12 +4,24 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+#this is for development purposes only,
+app.config['SECRET_KEY'] = 'Its the end of the world as we know it and I feel fine'
+
+#for non-dev, store a key en env and use:
+#app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
+
+app.debug = True
+
 @app.route('/')
 def home():  # put application's code here
     return render_template('index.html', name='Tom')
+
+
 @app.route('/data')
 def data():  # put application's code here
     return render_template('data.html', name='Tom')
+
+
 @app.route('/viz')
 def viz():  # put application's code here
     return render_template('viz.html', name='Tom')
@@ -18,7 +30,6 @@ def viz():  # put application's code here
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
 
 
 if __name__ == '__main__':
